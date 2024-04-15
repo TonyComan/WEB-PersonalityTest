3ce6f31eae726d3140d99c9c7b3ce6a71c2ab8b4
TonyComan <139473548+TonyComan@users.noreply.github.com>
2024-04-15 22:14:28 +0300

    Test 22:15
    Nu este Complet!!!

---
+++ Test Personalitate/ButonAuten.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+   
+    
+    console.log("Username:", username);
+    console.log("Password:", password);
+  
+    
+    window.location.href = "HomePage.html";
+  });
+  
